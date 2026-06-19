4a82f0f73f3c4ec7665e2a05f02b7a8cedc20b5e
skpgelo <skpgelo137@gmail.com>
2026-06-19 23:49:47 +0700

    Update header text for registration form

--- index.php
+++ index.php
@@ -8,14 +8,15 @@
     <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"> -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
-<body>
 
 <body class="bg-light">
 
 <div class="container py-5">
     <div class="card shadow-sm">
         <div class="card-header bg-primary text-white">
-            <h5 class="mb-0">Form Pendaftaran</h5>
+            <h5 class="mb-0">PELAKSANAAN TUGAS POKJA LAYANAN SOSIAL SUB POKJA DSM DAN PENDAMPINGAN SOSIAL</h5>
+            <h5 class="mb-0">KOORDINATOR DESA SEJAHTERA MANDIRI (DSM)</h5>
+            <h6 class="mb-0"><p id="tanggal"></p></h6>
         </div>
         <div class="card-body">
             <form id="wfhForm">
